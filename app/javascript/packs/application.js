a17ffc96bef3b34c7f1be4d6790ee96b7c9788ba
hisnuinn333 <tatsuro11111133@gmail.com>
2020-06-21 09:38:35 +0900

    メモ投稿非同期

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,6 +1,8 @@
-require("@rails/ujs").start();
-require("turbolinks").start();
-require("@rails/activestorage").start();
-require("channels");
-require("../checked");
-require("../memo");
+// require("@rails/ujs").start();
+// require("turbolinks").start();
+// require("@rails/activestorage").start();
+// require("channels");
+// require("../checked");
+// require("../memo");
+
+// ※このファイルの記述の必要性がわからない
