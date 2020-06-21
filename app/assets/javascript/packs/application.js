31b815af1e05c8c37d46c7dfa9180567d707214a
hisnuinn333 <tatsuro11111133@gmail.com>
2020-06-21 13:25:57 +0900

    first commit

--- app/assets/javascript/packs/application.js
+++
@@ -1,8 +0,0 @@
-// require("@rails/ujs").start();
-// require("turbolinks").start();
-// require("@rails/activestorage").start();
-// require("channels");
-// require("../checked");
-// require("../memo");
-
-// ※このファイルの記述の必要性がわからない
